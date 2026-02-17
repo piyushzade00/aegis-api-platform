fcfbde5b4985b611ebbe20e5c86524e9f86b30c7
piyushzade00 <piyushzade00@gmail.com>
2026-02-17 23:57:44 +0530

    chore: initialize project structure

---
+++ aegis-api-platform/src/main/java/com/aegis/api_platform/AegisApiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aegis.api_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisApiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisApiPlatformApplication.class, args);
+	}
+
+}
---
+++ aegis-api-platform/src/test/java/com/aegis/api_platform/AegisApiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.api_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisApiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
