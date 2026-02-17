d9ccaad586f180e05afad173a676c91a3ff58dd9
piyushzade00 <piyushzade00@gmail.com>
2026-02-18 01:18:36 +0530

    feat: add Tenant entity and repo

---
+++ aegis-api-platform/src/main/java/com/aegis/api_platform/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.aegis.api_platform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    protected Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+}
---
+++ aegis-api-platform/src/main/java/com/aegis/api_platform/model/Tenant.java
@@ -0,0 +1,38 @@
+package com.aegis.api_platform.model;
+
+import com.aegis.api_platform.enums.Status;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tenant")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tenant extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    public Tenant(String name, Status status){
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Tenant name cannot be empty or blank.");
+        }
+
+        if (status == null) {
+            throw new IllegalArgumentException("Tenant status cannot be null.");
+        }
+
+        this.name = name;
+        this.status = status;
+    }
+}
